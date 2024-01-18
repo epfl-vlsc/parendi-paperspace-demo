95fd0942b7725e2e7eae5ed0295c9afb148fd502
Mahyar Emami <mayyxeng@gmail.com>
2024-01-18 17:47:00 +0000

    use graphcore's setup script

---
+++ prereqs.sh
@@ -0,0 +1,7 @@
+apt-get update
+# Install prereqs
+apt-get install -y git help2man perl python3 make ninja-build cmake autoconf g++ flex bison ccache
+apt-get install -y libgoogle-perftools-dev numactl perl-doc libboost-all-dev
+apt-get install -y g++-10 gcc-10 # needed on Ubuntu 20.04
+
+
