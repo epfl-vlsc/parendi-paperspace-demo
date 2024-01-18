2d8f7b6725a2a0f74aff4eba15ed21987f0ff15d
Mahyar Emami <mayyxeng@gmail.com>
2024-01-18 17:52:16 +0000

    merge build scripts

--- build_parendi.sh
+++ build_parendi.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+apt-get update
+# Install prereqs
+apt-get install -y git help2man perl python3 make ninja-build cmake autoconf g++ flex bison ccache
+apt-get install -y libgoogle-perftools-dev numactl perl-doc libboost-all-dev
+apt-get install -y g++-10 gcc-10 # needed on Ubuntu 20.04
+
+
 # Clone the repository
 echo "Cloning Parendi"
 git clone https://github.com/epfl-vlsc/parendi.git
-git submodule update --init --recursive
 cd parendi
+git submodule update --init --recursive
 mkdir -p build
 cd build
 unset VERILATOR_ROOT # PARENDI does not rely on it
